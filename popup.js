ffba3edd6ff389c2f727f9820ec866ecc3dec646
Bjinin <92663097+Bjinin@users.noreply.github.com>
2024-04-24 23:34:38 +0900

    popup css

--- popup.js
+++ popup.js
@@ -1,8 +1,8 @@
-// 아이콘 클릭 시 팝업 열기
 function openPopup(content) {
     document.getElementById("popupTitle").innerText = content;
     document.getElementById("popupContent").innerText = "팝업 내용: " + content;
     document.getElementById("popupContainer").style.display = "block";
+    window.open('popup.html', 'Popup', 'width=600,height=400');
 }
 
 // 팝업 닫기
